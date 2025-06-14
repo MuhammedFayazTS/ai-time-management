81425e364d739069104d125a8018d275230f5517
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2025-06-14 09:37:25 +0530

    refactor: update in project structure

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,16 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      // authorize: 
+    }),
+  ],
+});
